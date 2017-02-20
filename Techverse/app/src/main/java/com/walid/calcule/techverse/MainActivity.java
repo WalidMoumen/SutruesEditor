417f0cad397ce5b0b773ce7071420c7f8fea9c5c
WalidVi <walidmoumen2@gmail.com>
2017-02-21 00:06:49 +0100

    SutruesEditor1
    SutruesEditor1

---
+++ Techverse/app/src/main/java/com/walid/calcule/techverse/MainActivity.java
@@ -0,0 +1,22 @@
+package com.walid.calcule.techverse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private CanvasView canvasView ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        canvasView = (CanvasView) findViewById(R.id.canvas) ;
+    }
+
+    public void clearCanvas(View v){
+        canvasView.clearCanvas();
+    }
+}
